490f43aa89527e5a9b5e705bf43dacc5e16585ab
Roland Vachter <roland.vachter@ft.com>
2016-03-17 21:54:56 +0200

    Fix lcd display.

--- app/modules/lcd.js
+++ app/modules/lcd.js
@@ -19,15 +19,14 @@ if (env.lcd.enable) {
 	});
 
 	let printLcd = function (data) {
-		lcd.clear();
 		lcd.setCursor(0, 0);
 		lcd.print('I: '+
-					("     " + data.inside.temperature.toFixed(1)).slice(-5) + '°C  ' +
+					("     " + data.inside.temperature.toFixed(1)).slice(-5) + 'C  ' +
 					("   " + data.inside.humidity).slice(-3) + '%',
 			() => {
 				lcd.setCursor(0, 1);
 				lcd.print('O: '+
-					("     " + data.outside.temperature.toFixed(1)).slice(-5) + '°C  ' +
+					("     " + data.outside.temperature.toFixed(1)).slice(-5) + 'C  ' +
 					("   " + data.outside.humidity).slice(-3) + '%');
 			}
 		);
