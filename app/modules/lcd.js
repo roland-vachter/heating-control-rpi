83fc03323ca492ac2e5cf50cc3b4a3f40f0c2552
Roland Vachter <roland.vachter@ft.com>
2016-03-18 19:24:13 +0200

    Fix LCD.

--- app/modules/lcd.js
+++ app/modules/lcd.js
@@ -26,6 +26,7 @@ if (env.lcd.enable) {
 		let printLcd = function (data) {
 			lcd.clear(function () {
 				lcd.home(function () {
+					lcd.setCursor(0, 0);
 					lcd.print('I: '+
 								("     " + data.inside.temperature.toFixed(1)).slice(-5) + 'C  ' +
 								("   " + data.inside.humidity).slice(-3) + '%',
